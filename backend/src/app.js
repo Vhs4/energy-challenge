8bd55bd3b1113aeaab299cba4f86beca3778f5f7
Vhs4 <vhszera44444@gmail.com>
2025-02-16 13:24:44 -0300

    FEAT: FIX CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ const supplierRoutes = require("./routes/supplierRoutes")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json())
 
 app.use("/api/suppliers", supplierRoutes)
