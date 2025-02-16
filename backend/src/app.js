f875ed5e5d3457eeea60bb89f5a3587a63dfe567
Vhs4 <vhszera44444@gmail.com>
2025-02-16 13:27:25 -0300

    FEAT: Deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,7 @@ const supplierRoutes = require("./routes/supplierRoutes")
 
 const app = express()
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
+app.use(cors())
 app.use(express.json())
 
 app.use("/api/suppliers", supplierRoutes)
